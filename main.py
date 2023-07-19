640b491d4a165f6411a0872ebfaea885ed5574e8
Jakub Zbroiński <kubazbroinsk@gmail.com>
2023-07-19 11:32:29 +0200

    dziala

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("hello word")
+import math
+#print("hello word")
+
+#jedznie1=float(input("Ile kosztuje pierwszy produkt?"))
+#jedznie2=float(input("Ile kosztuje drugi produkt?"))
+#jedznie3=float(input("Ile kosztuje trzeci produkt?"))
+#print(f"czy produkt 1 jest droższy od 2 ? {jedznie1>jedznie2} czy prukt 2 jest droższy od 3? {jedznie2>jedznie3}")
+#lista_zakupow=input("wypisz liste zakupów ale prosze od przecinków bez spacji")
+#odzielona=lista_zakupow.split(",")
+#nie mogłem wytzymac i zrobiłem to ifem  choc nie miałem
+#if len(odzielona)>5:
+ #   print("długa lista")
+#else:
+#    print("Krótka lista")
+
+
+#if elsy elsy
+#if warunek1:
+ #   instrukcja1
+#elif warunek2:
+#    instrukcja2
+#else:
+#    instrukcja3
+
+print("obliczymy czy zdałes do kolejnej klasy!")
+ocena1=float(input("podaj 1 ocene"))
+ocena2=float(input("podaj 2 ocene"))
+ocena3=float(input("podaj 3 ocene"))
+ocena4=float(input("podaj 4 ocene"))
+ocena5=float(input("podaj 5 ocene"))
+ocena6=float(input("podaj 6 ocene"))
+oceny=[ocena6,ocena5,ocena4,ocena3,ocena2,ocena1]
+srednia=(ocena6+ocena5+ocena4+ocena3+ocena2+ocena1)/float(len(oceny))
+if srednia>2:
+    print("zdałes!")
+else:
+    print("nie dałes nie robie!")
+
